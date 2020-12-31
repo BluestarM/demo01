e71694428d41d65ae4804a61042dc48996dcae9f
BluestarM <1016235180@qq.com>
2020-12-31 17:41:09 +0800

    创建项目

---
+++ pages/index/index.vue
@@ -0,0 +1,160 @@
+<template>
+	<view class="content">
+		<view class="avatorWrapper">
+			<view class="avator">
+				<image class="img" src="../../static/logo.png" mode="widthFix"></image>
+			</view>
+		</view>
+		<view class="form">
+			<view class="inputWrapper">
+				<input class="input" type="text" v-model="username" value="" placeholder="请输入用户名" />
+			</view>
+			<view class="inputWrapper">
+				<input class="input" type="password" v-model="password" value="" placeholder="请输入密码" />
+			</view>
+			<view class="loginBtn">
+				<text v-on:click="submit" class="btnValue">登录</text>
+			</view>
+			<view class="forgotBtn">
+				<text v-on:click="fundPwd">找回密码</text>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			submit: function() {
+				if (this.username === undefined) {
+					uni.showToast({
+						icon: 'none',
+						title: '请输入用户名'
+					});
+					return;
+				}
+				if (this.password === undefined) {
+					uni.showToast({
+						icon: 'none',
+						title: '请输入密码'
+					});
+					return;
+				}
+				if (this.username !== 'admin') {
+					uni.showToast({
+						icon: 'none',
+						title: '用户名不正确'
+					});
+					return;
+				}
+				if (this.password !== '123456') {
+					uni.showToast({
+						icon: 'none',
+						title: '密码不正确'
+					});
+					return;
+				}
+				if (this.username === 'admin' && this.password === '123456') {
+					uni.showToast({
+						icon: 'none',
+						title: '登录成功'
+					});
+					uni.navigateTo({
+						url:'../tabBar/home/home'
+					});
+
+					return;
+				}
+			},
+			fundPwd: function() {
+					uni.navigateTo({
+						url:'./fundPwd'
+					});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		background: #009688;
+		;
+		width: 100vw;
+		height: 100vh;
+	}
+
+	.avatorWrapper {
+		height: 30vh;
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		align-items: flex-end;
+	}
+
+	.avator {
+		width: 200upx;
+		height: 200upx;
+		overflow: hidden;
+	}
+
+	.avator .img {
+		width: 100%
+	}
+
+	.form {
+		padding: 0 100upx;
+		margin-top: 80px;
+	}
+
+	.inputWrapper {
+		width: 100%;
+		height: 80upx;
+		background: white;
+		border-radius: 20px;
+		box-sizing: border-box;
+		padding: 0 20px;
+		margin-top: 25px;
+	}
+
+	.inputWrapper .input {
+		width: 100%;
+		height: 100%;
+		text-align: center;
+		font-size: 15px;
+	}
+
+	.loginBtn {
+		width: 100%;
+		height: 80upx;
+		line-height: 80upx;
+		background: #8BC34A;
+		border-radius: 50upx;
+		margin-top: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+	}
+
+	.loginBtn .btnValue {
+		color: white;
+		width: 100%;
+		height: 100%;
+		text-align: center;
+	}
+
+	.forgotBtn {
+		text-align: center;
+		color: #EAF6F9;
+		font-size: 15px;
+		margin-top: 20px;
+	}
+</style>
---
+++ pages/tabBar/admin/admin.vue
@@ -0,0 +1,112 @@
+<template>
+	<view class="container">
+		<view class="tx-w">
+			<view class="tx">
+				<image class="tx-img" :src="yonghuwx.avatarUrl"></image>
+				<view class="name">{{yonghuwx.nickName}} </view>
+				<view class="zx"></view>
+			</view>
+			<view class="name-qm">
+				<view class="name-hei">ENJOY黑卡</view>
+				<view class="name-new">黑卡权益上新啦</view>
+				<view class="name-ka">开通会员</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				yonghuwx: []
+			}
+		},
+		onLoad() {
+			let that = this;
+			uni.login({
+				provider: 'weixin',
+				success: function(loginRes) {
+					// 获取用户信息				
+					uni.getUserInfo({
+						provider: 'weixin',
+						success: function(infoRes) {
+							that.yonghuwx = infoRes.userInfo
+							console.log(that.yonghuwx)
+						}
+					});
+				}
+			});
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.tx-w {
+		padding: 20upx 20upx 0;
+		background-image: linear-gradient(#0bc5a3, #00BCD4);
+	}
+
+	.tx {
+		color: #FFFFFF;
+		font-weight: bold;
+	}
+
+	.tx image {
+		float: left;
+		width: 140upx;
+		height: 140upx;
+		border-radius: 50%;
+	}
+
+	.name {
+		float: left;
+		text-align: center;
+		margin-top: 60upx;
+		margin-left: 20upx;
+	}
+
+	.zx {
+		clear: both;
+	}
+
+	.name-qm {
+		margin-top: 30upx;
+		padding: 0upx 20upx;
+		font-size: 30upx;
+		height: 90upx;
+		line-height: 90upx;
+		background: #333;
+		color: #ecc32e;
+		border-top-left-radius: 25upx;
+		border-top-right-radius: 25upx;
+	}
+
+	.name-hei,
+	.name-new,
+	.name-ka {
+		float: left;
+	}
+
+	.name-new {
+		font-size: small;
+		margin-left: 130upx;
+	}
+
+	.name-ka {
+		margin-top: 15rpx;
+		margin-left: 20rpx;
+		padding-left: 20rpx;
+		font-size: 28rpx;
+		width: 160rpx;
+		font-weight: bold;
+		height: 60rpx;
+		line-height: 60rpx;
+		background: #ecc32e;
+		color: #333;
+		border-radius: 30rpx;
+	}
+</style>
